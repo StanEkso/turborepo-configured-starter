d4bd83dea32098f7429f117226bba9005e15eb21
Stanislav <eksodev@gmail.com>
2024-01-31 01:22:06 +0300

    feat: add configuration for shared library

---
+++ packages/shared/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: false,
+  clean: true,
+  tsconfig: "tsconfig.build.json",
+  format: ["cjs", "esm"],
+  dts: true,
+});
